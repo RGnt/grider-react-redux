8b5621773ba9c821273fbfaa2396fc79ddce2329
Riku-Juhani Niemelä <rjniem@gmail.com>
2023-04-01 15:47:54 +0300

    1st UserForm Test done

--- react-testing-library/users/src/UserForm.test.jsx
+++ react-testing-library/users/src/UserForm.test.jsx
@@ -2,7 +2,15 @@ import {render, screen} from "@testing-library/react";
 import UserForm from "./UserForm.jsx";
 
 describe('UserForm', () => {
-    it('renders', () => {
-        render(<UserForm onUserAdd={() => {}/>)
+    it('shows two inputs and a button', () => {
+        // Render the component
+        render(<UserForm onUserAdd={() => {}}/>)
+
+        // Manipulate the component or find tan element in it
+        const inputs = screen.getAllByRole('textbox')
+        const button = screen.getByRole('button')
+        // Assertions - Make sure the component is doing what we expect it to do
+        expect(inputs).toHaveLength(2);
+        expect(button).toBeInTheDocument();
     })
 })
